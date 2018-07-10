f10b6fd763045969698a0922600ce3797bfe664f
FrenzyLi <fypetro@gmail.com>
2018-07-10 18:45:19 +0800

    Parse CLI --key value pair

--- README.md
+++ README.md
@@ -14,13 +14,18 @@ Install the prerequisites then add relevant directories to the system-wide `Path
 # Examples
 Generate the logo:
 
-    anilatex.py "Ani\LaTeX" -D 600 -o anilatex.png
+    anilatex.py "Ani\LaTeX" -D 600 -o anilatex
 
 LaTeX support: ![One proof that pi is bounded upwards by 22/7](https://github.com/FYPetro/AniLaTeX/blob/master/image/pi-bound.png)
 
-    anilatex.py "$\displaystyle 0<\int_{0}^{1}\frac{x^{4}(1-x)^{4}}{1+x^{2}}\ dx=\frac{22}{7}-\pi$" -o pi-bound.png
+    anilatex.py "$\displaystyle 0<\int_{0}^{1}\frac{x^{4}(1-x)^{4}}{1+x^{2}}\ dx=\frac{22}{7}-\pi$" -o pi-bound
 
 CJK support: ![CJK support](https://github.com/FYPetro/AniLaTeX/blob/master/image/cjk.png)
 
-    anilatex.py "{\Chi 中文与}{\Jpn 日本語と}{\Kor 한국어}" -o cjk.png -t standalone-cjk
+    anilatex.py "{\Chi 中文与}{\Jpn 日本語と}{\Kor 한국어}" -o cjk -t standalone-cjk
 
+`bclogo` package wrapper:
+
+![bclogo example](https://github.com/FYPetro/AniLaTeX/blob/master/image/bclogo.png)
+
+    anilatex.py -D 72 -o bclogo -t bclogo "LaTeX is a document preparation system. When writing, the writer uses plain text as opposed to the formatted text found in WYSIWYG (``what you see is what you get'') word processors like Microsoft Word, LibreOffice Writer and Apple Pages. The writer uses markup tagging conventions to define the general structure of a document (such as article, book, and letter) to stylise text throughout a document (such as bold and italics), and to add citations and cross-references. A TeX distribution such as TeX Live or MikTeX is used to produce an output file (such as PDF or DVi) suitable for printing or digital distribution. Within the typesetting system, its name is stylised as \LaTeX. This paragraph was taken from Wikipedia on \today." --title "What is \LaTeX?" --option "logo={\bcattention}"
--- anilatex.py
+++ anilatex.py
@@ -3,6 +3,7 @@
 import argparse
 import configparser
 import errno
+import logging
 import os
 import re
 import subprocess
@@ -11,7 +12,7 @@ from shutil import copy2
 # Global variables.
 config = configparser.ConfigParser()
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-DEFAULT_DPI = 72
+DEFAULT_DPI = 42
 if os.name == 'nt':
     DEFAULT_FONT = {
         'chinese': 'SimSun',
@@ -28,48 +29,51 @@ else:
 
 # Global regular expressions.
 REGEX = {
-    'print': r'显示“(.*)”。'
+    'print': r'显示“(.*)”。',
+    'parameter': r'^--([a-zA-Z][a-zA-Z0-9]*)$'
 }
 
 
-def print_latex(body, workspace, template=None, preamble=None, filename='temp', dpi=None):
+def print_latex(body, workspace, template=None, filename='temp', dpi=None, placeholder={}):
     """Produce and compile a standalone LaTeX file"""
     document = """\\documentclass[preview,margin=1pt]{standalone}
 \\usepackage{amsmath}
-\\input{anilatex-preamble}
+\\input{AniLaTeX-preamble}
 \\begin{document}
-\\input{anilatex-body}
+\\input{AniLaTeX-body}
 \\end{document}
 """
+    if 'preamble' not in placeholder:
+        placeholder['preamble'] = ''
+
     if template is not None:
         template_path = os.path.join(PROJECT_DIR, 'template', '{}.tex'.format(template))
         with open(template_path, 'r', encoding='utf-8') as file:
             document = file.read()
-    document = document.replace('\\input{anilatex-CJKChinese}', DEFAULT_FONT['chinese'])
-    document = document.replace('\\input{anilatex-CJKJapanese}', DEFAULT_FONT['japanese'])
-    document = document.replace('\\input{anilatex-CJKKorean}', DEFAULT_FONT['korean'])
-    document = document.replace('\\input{anilatex-preamble}', preamble or '')
-    document = document.replace('\\input{anilatex-body}', body)
+
+    document = document.replace('\\input{AniLaTeX-CJKChinese}', DEFAULT_FONT['chinese'])
+    document = document.replace('\\input{AniLaTeX-CJKJapanese}', DEFAULT_FONT['japanese'])
+    document = document.replace('\\input{AniLaTeX-CJKKorean}', DEFAULT_FONT['korean'])
+    document = document.replace('\\input{AniLaTeX-body}', body)
+    for key, value in placeholder.items():
+        document = document.replace('\\input{AniLaTeX-' + key + '}', value)
+
     tex_path = os.path.join(workspace, '{}.tex'.format(filename))
     pdf_path = os.path.join(workspace, '{}.pdf'.format(filename))
     png_path = os.path.join(workspace, '{}.png'.format(filename))
     with open(tex_path, 'w', encoding='utf-8') as file:
         file.write(document)
-    #with open(os.devnull, 'w') as devnull:
     tex_backend = 'xelatex'
-    subprocess.run([tex_backend, tex_path],
-        cwd=workspace)#, stdout=devnull)
+    subprocess.run([tex_backend, tex_path], cwd=workspace)
     dpi = dpi or DEFAULT_DPI
     gs_backend = 'gs'
     if os.name == 'nt':
         gs_backend = 'gswin64c'
     subprocess.run([gs_backend, '-dSAFER', '-dBATCH', '-dNOPAUSE',
-        '-r{}'.format(dpi*3), '-DownScaleFactor={}'.format(3), '-dGraphicsAlphaBits=4', '-sDEVICE=pngalpha',
+        '-r{}'.format(dpi*3), '-DownScaleFactor={}'.format(3),
+        '-dGraphicsAlphaBits=4', '-sDEVICE=pngalpha',
         '-sOutputFile={}'.format(png_path), pdf_path],
-        cwd=workspace)#, stdout=devnull)
-    # subprocess.run(['dvipng', dvi_path,
-    #     '-o', png_path, '-D', '{}'.format(dpi or DEFAULT_DPI)],
-    #     cwd=workspace)#, stdout=devnull)
+        cwd=workspace)
 
 
 def parse_animath(path):
@@ -81,7 +85,7 @@ def parse_animath(path):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    
+
     with open(path, 'r', encoding='utf-8') as file:
         commands = file.read().splitlines()
         for command in commands:
@@ -109,12 +113,25 @@ if __name__ == '__main__':
         help='name of template file', metavar='template_file', dest='template')
     parser.add_argument('--demo', nargs='?', const='hello_world',
         help='name of demo to be executed', metavar='demo_name')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
 
     if args.output is None:
         args.output = 'text'
-    
+
+    # Parse --key value pairs
+    parameter = {}
+    truly_unknown = []
+    for index in range(0,len(unknown)):
+        scurrent = re.search(REGEX['parameter'], unknown[index])
+        if scurrent and index + 1 < len(unknown) and not re.search(REGEX['parameter'], unknown[index + 1]):
+            key = scurrent.group(1)
+            value = unknown[index + 1]
+            parameter[scurrent.group(1)] = unknown[index + 1]
+        else:
+            truly_unknown.append(unknown[index])
+
     if args.text is not None:
+        """Turn text into still image"""
         directory = os.path.join(PROJECT_DIR, 'demo', 'ws')
         png_path = os.path.join(directory, '{}.png'.format(args.output))
         try:
@@ -123,12 +140,16 @@ if __name__ == '__main__':
             if e.errno != errno.EEXIST:
                 raise
         print_latex(args.text, directory,
-            filename=args.output, dpi=args.outres, template=args.template)
+            filename=args.output, dpi=args.outres, template=args.template,
+            placeholder=parameter)
         copy2(png_path, os.getcwd())
     elif args.demo is not None:
+        """Turn AniLaTeX demo file into still image"""
         path = os.path.join(PROJECT_DIR, 'demo', args.demo)
         parse_animath(path)
     elif args.input is not None:
+        """Turn AniLaTeX file into still image"""
         parse_animath(args.input)
     else:
+        """On the top of the hill is a lonely help page"""
         parser.print_help()
