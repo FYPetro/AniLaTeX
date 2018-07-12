063d05be38dcf24cbeaac5412c3ec2a5271c547a
FrenzyLi <fypetro@gmail.com>
2018-07-12 17:46:42 +0800

    Overhaul section and placeholder logic

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ LaTeX support: ![One proof that pi is bounded upwards by 22/7](https://github.co
 
 CJK support: ![CJK support](https://github.com/FYPetro/AniLaTeX/blob/master/image/cjk.png)
 
-    anilatex.py "{\Chi 中文与}{\Jpn 日本語と}{\Kor 한국어}" -cjk -o cjk
+    anilatex.py "{\Chi 中文与}{\Jpn 日本語と}{\Kor 한국어}" -p cjk -o cjk
 
 `bclogo` package wrapper:
 
 ![bclogo example](https://github.com/FYPetro/AniLaTeX/blob/master/image/bclogo.png)
 
-    anilatex.py "LaTeX is a document preparation system. When writing, the writer uses plain text as opposed to the formatted text found in WYSIWYG (``what you see is what you get'') word processors like Microsoft Word, LibreOffice Writer and Apple Pages. The writer uses markup tagging conventions to define the general structure of a document (such as article, book, and letter) to stylise text throughout a document (such as bold and italics), and to add citations and cross-references. A TeX distribution such as TeX Live or MikTeX is used to produce an output file (such as PDF or DVi) suitable for printing or digital distribution. Within the typesetting system, its name is stylised as \LaTeX. {\Kor 위 단락} {\Chi 节选自} {\Jpn ウイキペディア} on \today." --title "What is \LaTeX?" --option "logo={\bcattention}" -cjk -t bclogo -D 72 -o bclogo
+    anilatex.py "LaTeX is a document preparation system. When writing, the writer uses plain text as opposed to the formatted text found in WYSIWYG (``what you see is what you get'') word processors like Microsoft Word, LibreOffice Writer and Apple Pages. The writer uses markup tagging conventions to define the general structure of a document (such as article, book, and letter) to stylise text throughout a document (such as bold and italics), and to add citations and cross-references. A TeX distribution such as TeX Live or MikTeX is used to produce an output file (such as PDF or DVi) suitable for printing or digital distribution. Within the typesetting system, its name is stylised as \LaTeX. {\Kor 위 단락} {\Chi 节选自} {\Jpn ウイキペディア} on \today." --title "What is \LaTeX?" --option "logo={\bcattention}" -p cjk -t bclogo -D 72 -o bclogo
--- anilatex.py
+++ anilatex.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 import argparse
 import configparser
+import datetime
 import errno
 import logging
 import os
 import re
 import subprocess
+from _version import __version__
 from shutil import copy2, rmtree
 
-# Global variables.
+# Global variables
 config = configparser.ConfigParser()
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_DPI = 42
@@ -27,7 +29,7 @@ else:
     }
 
 
-# Global regular expressions.
+# Global regular expressions
 REGEX = {
     'print': r'显示“(.*)”。',
     'parameter': r'^--([a-zA-Z][a-zA-Z0-9]*)$',
@@ -35,72 +37,82 @@ REGEX = {
     'section': r'(\\begin{AniLaTeX-(([a-zA-Z][a-zA-Z0-9]+?)(?::([0-9]+))?)}).*?(\\end{AniLaTeX-\2})'
 }
 
+def format_usepackage(package, options=[]):
+    """Formats package options as usepackage directives"""
+    if len(options) == 0:
+        return '\\usepackage{{{}}}'.format(package)
+    else:
+        return '\\usepackage[{}]{{{}}}'.format(','.join(options), package)
 
-def print_latex(body, workspace, filename='temp', template=None, cjk=False, dpi=None, placeholder={}):
+
+def print_latex(body, workspace, filename='temp', template=None, dpi=None, placeholder={}, package=[]):
     """Produce and compile a standalone LaTeX file"""
-    document = """\\documentclass[preview,margin=1pt]{standalone}
-\\usepackage{amsmath}
+    document = """\\documentclass[preview,margin=\\input{AniLaTeX-margin={0pt}}]{\\input{AniLaTeX-documentclass={standalone}}}
 \\input{AniLaTeX-preamble}
 \\begin{document}
 \\input{AniLaTeX-body}
 \\end{document}
 """
-    cjk_preamble = """\\usepackage{{fontspec}}
-\\usepackage{{xeCJK}}
-% Reference: https://tex.stackexchange.com/a/45403
-\\setCJKfamilyfont{{zhrm}}{{{}}}
-\\setCJKfamilyfont{{jarm}}{{{}}}
-\\setCJKfamilyfont{{korm}}{{{}}}
-\\newcommand\\Chi{{\\CJKfamily{{zhrm}}\\CJKnospace}}
-\\newcommand\\Jpn{{\\CJKfamily{{jarm}}\\CJKnospace}}
-\\newcommand\\Kor{{\\CJKfamily{{korm}}\\CJKspace}}
-""".format(DEFAULT_FONT['chinese'], DEFAULT_FONT['japanese'], DEFAULT_FONT['korean'])
+    # Set default argument for placeholders
     default_inputs = {
         'preamble': '',
+        'fontchi': DEFAULT_FONT['chinese'],
+        'fontjpn': DEFAULT_FONT['japanese'],
+        'fontkor': DEFAULT_FONT['korean'],
+        'version': __version__,
+        'date': datetime.datetime.today().strftime('%Y-%m-%d')
     }
     for key, value in default_inputs.items():
         if key not in placeholder:
             placeholder[key] = value
 
-    if cjk is True:
-        placeholder['preamble'] = cjk_preamble + placeholder['preamble']
+    # Prepare usepackage directives
+    if len(package) > 0:
+        usepackage_preamble = os.linesep.join([format_usepackage(x[0],x[1:]) for x in package])
+        placeholder['preamble'] = os.linesep.join([usepackage_preamble, placeholder['preamble']])
 
+    # Use provided template
     if template is not None:
         template_path = os.path.join(PROJECT_DIR, 'template', '{}.tex'.format(template))
         with open(template_path, 'r', encoding='utf-8') as file:
             document = file.read()
 
-    document = document.replace('\\input{AniLaTeX-CJKChinese}', DEFAULT_FONT['chinese'])
-    document = document.replace('\\input{AniLaTeX-CJKJapanese}', DEFAULT_FONT['japanese'])
-    document = document.replace('\\input{AniLaTeX-CJKKorean}', DEFAULT_FONT['korean'])
+    # Repeatedly turning off sections
+    replaced = None
+    while replaced is not False:
+        replaced = False
+        for s in re.finditer(REGEX['section'], document, flags=re.S):
+            replaced = True
+            key = s.group(3)
+            index = int(s.group(4) or '1')
+            # Body section:     \begin{AniLaTeX-body:1}
+            #                   \end{AniLaTeX-body:1}
+            # Placeholder text: \begin{AniLaTeX-key}
+            #                   \end{AniLaTeX-key}
+            if (key == 'body' and index > len(body)) or (not key == 'body' and not key in placeholder):
+                document = document.replace(s.group(0), '')
+            else:
+                document = document.replace(s.group(1), '').replace(s.group(5), '')
 
-    # Turn on or turn off a section.
-    for s in re.finditer(REGEX['section'], document, flags=re.S):
-        key = s.group(3)
-        index = int(s.group(4) or '1')
-        # Body section:     \begin{AniLaTeX-body:1}
-        #                   \end{AniLaTeX-body:1}
-        # Placeholder text: \begin{AniLaTeX-key}
-        #                   \end{AniLaTeX-key}
-        if (key == 'body' and index > len(body)) or (not key == 'body' and not key in placeholder):
-            document = document.replace(s.group(0), '')
-        else:
-            document = document.replace(s.group(1), '').replace(s.group(5), '')
+    # Repeatedly Replace all placeholders with specified value or default ones
+    replaced = None
+    while replaced is not False:
+        replaced = False
+        for s in re.finditer(REGEX['placeholder'], document):
+            replaced = True
+            key = s.group(1)
+            index = int(s.group(2) or '1')
+            default_value = s.group(3) or ''
+            value = None
+            # Body text:        \input{AniLaTeX-body:1={Default}}
+            if key == 'body' and 0 < index <= len(body):
+                value = body[index - 1]
+            # Placeholder text: \input{AniLaTeX-key={Default}}
+            elif key in placeholder:
+                value = placeholder[key]
+            document = document.replace(s.group(0), value or default_value)
 
-    # Replace all placeholders with specified value or default ones
-    for s in re.finditer(REGEX['placeholder'], document):
-        key = s.group(1)
-        index = int(s.group(2) or '1')
-        default_value = s.group(3) or ''
-        value = None
-        # Body text:        \input{AniLaTeX-body:1={Default}}
-        if key == 'body' and 0 < index <= len(body):
-            value = body[index - 1]
-        # Placeholder text: \input{AniLaTeX-key={Default}}
-        elif key in placeholder:
-            value = placeholder[key]
-        document = document.replace(s.group(0), value or default_value)
-
+    # Workflow: .tex --XeTeX-> .pdf
     tex_path = os.path.join(workspace, '{}.tex'.format(filename))
     pdf_path = os.path.join(workspace, '{}.pdf'.format(filename))
     png_path = os.path.join(workspace, '{}.png'.format(filename))
@@ -108,6 +120,8 @@ def print_latex(body, workspace, filename='temp', template=None, cjk=False, dpi=
         file.write(document)
     tex_backend = 'xelatex'
     subprocess.run([tex_backend, tex_path], cwd=workspace)
+
+    # Workflow: .pdf --GhostScript-> .png
     dpi = dpi or DEFAULT_DPI
     gs_backend = 'gs'
     if os.name == 'nt':
@@ -146,26 +160,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A python script to parse AniMath scripts.')
     parser.add_argument('text', nargs='*',
         help='text to write (bypass -i and --demo if provided)')
-    parser.add_argument('-clean', action='store_true',
+    parser.add_argument('-u', nargs='+', action='append',
+        help='use optional packages', dest='use')
+    parser.add_argument('-p', nargs='+',
+        help='use optional preambles', dest='preamble')
+    parser.add_argument('-c', action='store_true',
         help='clean the workspace', dest='clean')
     parser.add_argument('-D', nargs='?', type=int, const=DEFAULT_DPI,
-        help='output resolution of text', metavar='dpi', dest='outres')
+        help='output resolution of text', metavar='dpi', dest='dpi')
     parser.add_argument('-i', nargs='?',
         help='name of script to be parsed', metavar='file', dest='input')
     parser.add_argument('-o', nargs='?',
         help='name of output file', metavar='file', dest='output')
     parser.add_argument('-t', nargs='?',
         help='name of template file', metavar='template', dest='template')
-    parser.add_argument('-cjk', action='store_true',
-        help='enable xeCJK package', dest='cjk')
-    parser.add_argument('-demo', nargs='?', const='hello_world',
-        help='name of demo to be executed', metavar='demo_name')
+    parser.add_argument('-d', nargs='?', const='hello_world',
+        help='name of demo to be executed', metavar='demo')
     args, unknown = parser.parse_known_args()
 
+    # Set default output name
     if args.output is None:
         args.output = 'text'
 
-    # Parse --key value pairs
+    # Parse placeholder --key value pairs
     parameter = {}
     truly_unknown = []
     for index in range(0,len(unknown)):
@@ -179,8 +196,17 @@ if __name__ == '__main__':
         else:
             truly_unknown.append(unknown[index])
 
+    # Read and concatenate preambles
+    preamble = []
+    if args.preamble is not None:
+        for filename in args.preamble:
+            path = os.path.join(PROJECT_DIR, 'preamble', '{}.tex'.format(filename))
+            with open(path, 'r', encoding='utf-8') as file:
+                preamble.append(file.read())
+        parameter['preamble'] = os.linesep.join(preamble)
+
     if len(args.text) > 0:
-        """Turn text into still image"""
+        # Turn text into still image
         directory = os.path.join(PROJECT_DIR, 'demo', 'ws')
         png_path = os.path.join(directory, '{}.png'.format(args.output))
         try:
@@ -189,20 +215,20 @@ if __name__ == '__main__':
             if e.errno != errno.EEXIST:
                 raise
         print_latex(args.text, directory,
-            filename=args.output, dpi=args.outres, template=args.template,
-            cjk=args.cjk, placeholder=parameter)
+            filename=args.output, dpi=args.dpi, template=args.template,
+            placeholder=parameter, package=args.use or [])
         copy2(png_path, os.getcwd())
     elif args.demo is not None:
-        """Turn AniLaTeX demo file into still image"""
+        # Turn AniLaTeX demo file into still image
         path = os.path.join(PROJECT_DIR, 'demo', args.demo)
         parse_animath(path)
     elif args.input is not None:
-        """Turn AniLaTeX file into still image"""
+        # Turn AniLaTeX file into still image
         parse_animath(args.input)
+    elif not args.clean:
+        # On the top of the hill is a lonely help page
+        parser.print_help()
 
     if args.clean:
-        """Clean up"""
+        # Clean up
         rmtree(os.path.join(PROJECT_DIR, 'demo', 'ws'), ignore_errors=True)
-    else:
-        """On the top of the hill is a lonely help page"""
-        parser.print_help()
